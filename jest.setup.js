c89c711f2b08308fa4e8078be5298e00ef760a6e
Øswald Kardingson <164262873+OswaldKardingson@users.noreply.github.com>
2024-11-18 04:08:01 +0200

    Update jest.setup.js

--- jest.setup.js
+++ jest.setup.js
@@ -1,2 +1,8 @@
 // Mock the __DEV__ global variable used by React Native
 global.__DEV__ = true;
+
+// Mock the Platform module to resolve correctly in Jest
+jest.mock('react-native/Libraries/Utilities/Platform', () => ({
+  OS: 'ios', // Change to 'android' if testing Android-specific code
+  select: (options) => options.ios,
+}));
