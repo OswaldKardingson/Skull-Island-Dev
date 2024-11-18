abdf7a28cef4c3e488260fa3db7dcb9b10cbf22e
Øswald Kardingson <164262873+OswaldKardingson@users.noreply.github.com>
2024-11-18 03:09:29 +0200

    Update jest.config.js

--- jest.config.js
+++ jest.config.js
@@ -1,12 +1,13 @@
 module.exports = {
   transform: {
-    '^.+\\.[t|j]sx?$': 'babel-jest', // Transforms JavaScript and TypeScript files using Babel
+    '^.+\\.[tj]sx?$': 'babel-jest', // Transforms JavaScript and TypeScript files using Babel
   },
   transformIgnorePatterns: [
-    'node_modules/(?!(react-native|@react-native|react-native-vector-icons|@react-navigation)/)', 
-    // Transforms React Native and related libraries in node_modules
+    'node_modules/(?!(react-native|@react-native|react-native-vector-icons|@react-navigation)/)',
+    // Ensures React Native and related libraries in node_modules are transformed
   ],
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'], // File extensions Jest will resolve
-  setupFilesAfterEnv: ['@testing-library/jest-native/extend-expect'], // Adds custom matchers for testing React Native
-  testPathIgnorePatterns: ['<rootDir>/node_modules/', '<rootDir>/dist/'], // Ignore these paths
+  setupFilesAfterEnv: ['<rootDir>/node_modules/@testing-library/jest-native/extend-expect'], // Adds custom matchers for testing React Native
+  testPathIgnorePatterns: ['<rootDir>/node_modules/', '<rootDir>/dist/'], // Ignores these paths during testing
 };
+
