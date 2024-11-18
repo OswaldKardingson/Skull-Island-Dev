c131795f9074992d6c40734473446243d57e65cf
Øswald Kardingson <164262873+OswaldKardingson@users.noreply.github.com>
2024-11-18 15:03:04 +0200

    Update jest.config.js

--- jest.config.js
+++ jest.config.js
@@ -1,12 +1,11 @@
-// jest.config.js
 module.exports = {
   transform: {
-    '^.+\\.[t|j]sx?$': 'babel-jest',
+    '^.+\\.[tj]sx?$': 'babel-jest', // Transforms JavaScript and TypeScript files
   },
   transformIgnorePatterns: [
-    'node_modules/(?!(react-native|@react-native|react-native-vector-icons|@react-navigation)/)',
+    'node_modules/(?!(react-native|@react-native|react-native-vector-icons|@react-navigation)/)', // Transforms React Native-related modules
   ],
-  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
-  setupFilesAfterEnv: ['<rootDir>/jest.setup.js'], // Include setup file
-  testPathIgnorePatterns: ['<rootDir>/node_modules/', '<rootDir>/dist/'],
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'], // Extensions Jest will resolve
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.js'], // Include the setup file
+  testPathIgnorePatterns: ['<rootDir>/node_modules/', '<rootDir>/dist/'], // Ignore these paths
 };
