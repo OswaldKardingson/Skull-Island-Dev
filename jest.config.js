ee75cd5e8965bf87c7d899279685904dd60f66c8
Øswald Kardingson <164262873+OswaldKardingson@users.noreply.github.com>
2024-11-18 03:02:35 +0200

    Update jest.config.js

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,12 @@
 module.exports = {
   transform: {
-    "^.+\\.[t|j]sx?$": "babel-jest"
+    '^.+\\.[t|j]sx?$': 'babel-jest', // Transforms JavaScript and TypeScript files using Babel
   },
+  transformIgnorePatterns: [
+    'node_modules/(?!(react-native|@react-native|react-native-vector-icons|@react-navigation)/)', 
+    // Transforms React Native and related libraries in node_modules
+  ],
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'], // File extensions Jest will resolve
+  setupFilesAfterEnv: ['@testing-library/jest-native/extend-expect'], // Adds custom matchers for testing React Native
+  testPathIgnorePatterns: ['<rootDir>/node_modules/', '<rootDir>/dist/'], // Ignore these paths
 };
