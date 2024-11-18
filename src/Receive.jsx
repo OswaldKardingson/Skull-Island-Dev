030d0f52b1c3cb43fb00b4ea2b728ecf339dcb6a
Øswald Kardingson <164262873+OswaldKardingson@users.noreply.github.com>
2024-11-18 02:48:31 +0200

    Update Receive.jsx

--- src/Receive.jsx
+++ src/Receive.jsx
@@ -27,6 +27,7 @@ const Receive = ({ visible, theme }) => {
 
     return (
         <Animated.View
+            testID="container"
             style={[
                 styles.container,
                 {
