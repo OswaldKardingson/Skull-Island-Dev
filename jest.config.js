5331df63f2894ca43f4867444fc3eae1af6f173b
Øswald Kardingson <164262873+OswaldKardingson@users.noreply.github.com>
2024-11-18 04:13:01 +0200

    Update jest.config.js

--- jest.config.js
+++ jest.config.js
@@ -6,7 +6,7 @@ module.exports = {
     'node_modules/(?!(react-native|@react-native|react-native-vector-icons|@react-navigation)/)',
   ],
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
-  setupFiles: ['<rootDir>/jest.setup.js'], // Make sure this is included
+  setupFiles: ['<rootDir>/jest.setup.js'], // Ensure this is pointing to the right path
   setupFilesAfterEnv: ['@testing-library/jest-native/extend-expect'],
   testPathIgnorePatterns: ['<rootDir>/node_modules/', '<rootDir>/dist/'],
 };
